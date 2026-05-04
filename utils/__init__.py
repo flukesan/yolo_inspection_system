ef82bf72e35298ca735c956d0b6ff0efbc1480b0
flukesan <flukesan4503@gmail.com>
2026-05-04 22:39:52 +0700

    cleanup: remove v1 PyQt6 legacy files

--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Utilities package for YOLO Inspection System"""
-from .database import Database
-from .image_processor import ImageProcessor
-from .plc_communication import PLCCommunication
-from .report_generator import ReportGenerator
-
-__all__ = ['Database', 'ImageProcessor', 'PLCCommunication', 'ReportGenerator']
