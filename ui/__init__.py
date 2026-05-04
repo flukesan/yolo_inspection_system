0f2fe0cc4e7f36adfbc17afea97fd1b5938c2ed2
flukesan <flukesan4503@gmail.com>
2026-05-04 22:43:41 +0700

    cleanup: remove v1 PyQt6 legacy (~65 files)
    Deleted: core/, ui/, utils/, models/, root v1 scripts, old Dockerfiles, v1 configs/docs.
    All replaced by v2 web platform (edge/, server/, frontend/).

--- ui/__init__.py
+++
@@ -1,4 +0,0 @@
-"""UI package for YOLO Inspection System"""
-from .main_window import MainWindow
-
-__all__ = ['MainWindow']
--- ui/widgets/__init__.py
+++
@@ -1,8 +0,0 @@
-"""UI Widgets package"""
-from .camera_view import CameraView
-from .control_panel import ControlPanel
-from .statistics_panel import StatisticsPanel
-from .alert_panel import AlertPanel
-from .validation_result_panel import ValidationResultPanel
-
-__all__ = ['CameraView', 'ControlPanel', 'StatisticsPanel', 'AlertPanel', 'ValidationResultPanel']
