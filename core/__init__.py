ace573fe5ba127d20cce6d1a5de00460ffe971ba
flukesan <flukesan4503@gmail.com>
2026-05-04 22:39:38 +0700

    cleanup: remove v1 PyQt6 legacy files

--- core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Core components for YOLO Inspection System"""
-from .camera_manager import CameraManager
-from .yolo_detector import YOLODetector
-from .inspection_engine import InspectionEngine
-from .data_logger import DataLogger
-from .mqtt_client import MQTTClient
-
-__all__ = ['CameraManager', 'YOLODetector', 'InspectionEngine', 'DataLogger', 'MQTTClient']
